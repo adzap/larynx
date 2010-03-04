e205f9388be30a1dc284f70dce75066d3d29d2ac
Adam Meehan <adam.meehan@gmail.com>
2010-03-04 11:31:59 +1100

    removed EM cancel override which isn't changed

--- lib/freevoice/eventmachine.rb
+++ lib/freevoice/eventmachine.rb
@@ -1,13 +1,9 @@
 module EventMachine
+  # Adds restart to EM timer class
   class Timer
     def initialize(interval, callback=nil, &block)
       @interval, @callback, @block = interval, callback, block
       @signature = EventMachine::add_timer(interval, callback || block)
-    end
-
-    # Cancel the timer
-    def cancel
-      EventMachine.send :cancel_timer, @signature
     end
 
     # Restart the timer
