aa4b220be30f85f37e3ec0a4d8bc057aac6fa79b
Adam Meehan <adam.meehan@gmail.com>
2010-03-12 14:57:54 +1100

    added form setup block

--- lib/freevoice/form.rb
+++ lib/freevoice/form.rb
@@ -2,8 +2,18 @@ module Freevoice
   class Form < Application
     include Fields
 
+    def self.setup(&block)
+      @@setup = block
+    end
+
     def run
+      instance_eval &@@setup if @@setup
       next_field
+    end
+
+    def restart_form
+      @current_field = 0
+      run
     end
   end
 end
