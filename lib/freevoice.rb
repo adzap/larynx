7098dcb91dbea942b90c2bdf4e6c9a0cf92cf79a
Adam Meehan <adam.meehan@gmail.com>
2010-03-15 11:40:00 +1100

    option parsing for binary with daemonisation

--- lib/freevoice.rb
+++ lib/freevoice.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'eventmachine'
 require 'active_support'
+require 'daemons'
 
 require 'freevoice/eventmachine'
 require 'freevoice/observable'
@@ -21,16 +22,33 @@ module Freevoice
 
     define_callback :connect, :answer, :hungup
 
-    def start_server(ip="0.0.0.0", port=8084)
+    def start_server(ip, port)
       EM::run {
         EM::start_server ip, port, Freevoice::CallHandler
       }
+    end
+
+    def parse_options(args=ARGV)
+      options = {:ip => "0.0.0.0", :port => 8084}
+      opts = OptionParser.new
+      opts.banner = "Usage: freevoice [options]"
+      opts.separator ''
+      opts.separator "Freevoice is a tool to develop FreeSWITCH IVR applications in Ruby."
+      opts.on('-d', '--daemonize', 'Run as daemon')                     { options[:daemonize] = true }
+      opts.on('-i', '--ip IP',     'Listen for connections on this IP') {|ip| options[:ip] = ip }
+      opts.on('-p', '--port PORT', 'Listen on this port', Integer)      {|port| options[:port] = port }
+      opts.on('-h', '--help',      'This is it')                        { $stderr.puts opts; exit 0 }
+      opts.on('-v', '--version')                                        { $stderr.puts "freevoice version #{Freevoice::VERSION}"; exit 0 }
+      opts.parse!(args)
+      options
     end
   end
 end
 
 
 unless defined?(TEST)
+  options = Freevoice.parse_options
   require ARGV[0]
-  Freevoice.start_server
+  Daemons.daemonize if options[:daemonize]
+  Freevoice.start_server(options[:ip], options[:port])
 end
