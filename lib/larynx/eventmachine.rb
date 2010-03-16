2cbe0cb3878511182f59e26d0eca5af038d6d1ef
Adam Meehan <adam.meehan@gmail.com>
2010-03-16 11:31:45 +1100

    only one ivar to hold timer callback

--- lib/larynx/eventmachine.rb
+++ lib/larynx/eventmachine.rb
@@ -2,14 +2,15 @@ module EventMachine
   # Adds restart to EM timer class
   class Timer
     def initialize(interval, callback=nil, &block)
-      @interval, @callback, @block = interval, callback, block
-      @signature = EventMachine::add_timer(interval, callback || block)
+      @interval = interval
+      @callback = callback || block
+      @signature = EventMachine::add_timer(interval, @callback)
     end
 
     # Restart the timer
     def restart
       cancel
-      @signature = EventMachine::add_timer(@interval, @callback || @block)
+      @signature = EventMachine::add_timer(@interval, @callback)
     end
   end
 end
