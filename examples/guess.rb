e119bd6a835e9d7463308599414f8b9f3a76cc93
Adam Meehan <adam.meehan@gmail.com>
2010-03-24 21:08:57 +1100

    added guessing game examples in plain and form version

---
+++ examples/guess.rb
@@ -0,0 +1,42 @@
+class Guess < Larynx::Application
+	def run
+		@number = rand(9) + 1
+    @guess = ''
+		@guesses = 0
+		get_guess
+	end
+
+	def get_guess
+    $stdout.puts @guesses
+		if @guesses < 3
+			speak(guess_prompt) {
+				@guesses += 1
+			}
+		else
+			speak "Sorry you didn't guess it. It was #{@number}. Try again soon.", :bargein => false
+			hangup
+		end
+	end
+
+	def check_guess
+		if @guess.to_i == @number
+			speak "You got it! It was #{@guess}. It took you #{@guesses} guesses.", :bargein => false
+			speak "Thanks for playing."
+			hangup
+		else
+			speak "No it's not #{@guess}."
+			get_guess
+		end
+	end
+
+  def guess_prompt
+    @guesses == 0 ? 'Guess a number between 1 and 9.' : 'Have another guess.'
+  end
+
+	def dtmf_received(input)
+		@guess = input
+		check_guess
+	end
+end
+
+Larynx.answer {|call| Guess.run(call) }
---
+++ examples/guess_form.rb
@@ -0,0 +1,34 @@
+class Guess < Larynx::Form
+	field(:guess, :attempts => 3, :length => 1) do
+		prompt :speak => 'Guess a number between 1 and 9.'
+		reprompt :speak => 'Have another guess.'
+
+		setup do
+			@number = rand(9) + 1
+			@guesses = 0
+		end
+
+		validate do
+			@guesses += 1 if guess.size > 0
+			@number == guess.to_i
+		end
+
+		invalid do
+			if guess.size > 0
+				speak "No, it's not #{guess}.", :bargein => false
+			end
+		end
+
+		success do
+			speak "You got it! It was #{guess}. It took you #{@guesses} guesses.", :bargein => false
+			hangup
+		end
+
+		failure do
+			speak "Sorry you didn't guess it. It was #{@number}. Try again soon.", :bargein => false
+			hangup
+		end
+	end
+end
+
+Larynx.answer {|call| Guess.run(call) }
