4b6835a1a6b209d4c9e80608e6d892250e9ba87b
Adam Meehan <adam.meehan@gmail.com>
2010-09-13 12:41:37 +1000

    add AS dependencies

--- lib/larynx.rb
+++ lib/larynx.rb
@@ -3,6 +3,8 @@ require 'eventmachine'
 require 'active_support'
 require 'active_support/core_ext/module'
 require 'active_support/core_ext/class'
+require 'active_support/core_ext/hash'
+require 'active_support/core_ext/string'
 
 require 'larynx/version'
 require 'larynx/logger'
